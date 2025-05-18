6f390336742cffef7dfaad29bf95ad0c3aeb798a
RAJ MONDAL <rajmondal1907@gmail.com>
2025-05-18 15:27:31 +0530

    Moved Day 1 Problem 1 to Day1 folder

---
+++ Day1/problem1.py
@@ -0,0 +1,7 @@
+iceCreamPrices = [10,20,30,40,50]
+budget = 30
+
+def checkIceCreamPrice(iceCreamPrices,bugdet):
+   return budget in iceCreamPrices
+
+print(checkIceCreamPrice(iceCreamPrices,budget))
